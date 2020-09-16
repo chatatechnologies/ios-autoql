d799cc8ddd4446dc818e2ee3e984dfcbce36ce03
Vicente Rincón <vicente@rinro.com.mx>
2020-09-16 13:03:21 -0500

    Query Output part 2

--- Classes/QueryOutput.swift
+++ Classes/QueryOutput.swift
@@ -8,32 +8,20 @@
 import Foundation
 public class QueryOutput: UIView{
     public var authenticationInput: authentication = authentication()
-    public var autoQLConfig: autoQLConfigInput = autoQLConfigInput()
-    public var themeConfig: themeConfigInput = themeConfigInput()
-    public var isDisabled: Bool = false
-    public var placeholder: String = "Type Your Queries here"
-    public var showLoadingDots: Bool = true
-    public var showChataIcon: Bool = true
-    public var enableVoiceRecord: Bool = true
-    public var autoCompletePlacement: String = "above"
-    public var inputPlacement: String = "top"
-    /*
-     queryResponse (Required)    Object    -
-     queryInputRef    Instance of <QueryInput/>    undefined
-     displayType    String    undefined
-     height    Number    undefined
-     width    Number    undefined
-     onDataClick    Function    (groupByObject, queryID, activeKey) => {}
-     activeChartElementKey    String    undefined
-     renderTooltips    Boolean    true
-     onQueryValidationSelectOption    Function    () => {}
-     autoSelectQueryValidationSuggestion    Boolean    true
-     queryValidationSelections    Array of Suggestion Objects    undefined
-     renderSuggestionsAsDropdown    Boolean    false
-     suggestionSelection    String    undefined
-     dataFormatting    Object    {}
-     themeConfig
-     */
+    
+    
+    public var queryResponse: [String: Any] = [:]
+    //public var queryInputRef = nil
+    public var displayType: String = ""
+    public var height: Int = 0
+    public var width: Int = 0
+    public var activeChartElementKey: String = ""
+    public var renderTooltips: Bool = true
+    public var autoSelectQueryValidationSuggestion: Bool = true
+    public var queryValidationSelections : String = ""
+    public var renderSuggestionsAsDropdown: Bool = false
+    public var suggestionSelection: String
+    //public var dataFormatting: [S
     let tfMain = UITextField()
     let btnSend = UIButton()
     let tbAutoComplete = UITableView()
@@ -45,10 +33,10 @@ public class QueryOutput: UIView{
     public override init(frame: CGRect) {
         super.init(frame: frame)
     }
-    public func start(mainView: UIView) {
-        let finalAlign: DViewSafeArea = inputPlacement == "top" ? .topPadding : .bottomPadding
+    public func start(mainView: UIView, subView: UIView) {
         mainView.addSubview(self)
-        self.edgeTo(mainView, safeArea: finalAlign, height: 60 )
+        self.edgeTo(mainView, safeArea: .bottomPaddingtoTop, subView)
+        self.backgroundColor = .black
         DataConfig.authenticationObj = self.authenticationInput
         wsUrlDynamic = self.authenticationInput.domain
         generateComponents()
