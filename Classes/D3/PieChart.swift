511a8cfff79813ad1a4b910e83034d332732a060
Vicente Rincón <vicente@rinro.com.mx>
2020-10-04 14:03:49 -0500

    pie chart part 3

--- Classes/D3/PieChart.swift
+++ Classes/D3/PieChart.swift
@@ -22,7 +22,7 @@ func getPieChart() {
         <script>
 
         // set the dimensions and margins of the graph
-        var width = 450
+        var width = 650
             height = 450
             margin = 40
 
@@ -36,7 +36,6 @@ func getPieChart() {
             .attr("height", height)
           .append("g")
             .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
         // Create dummy data
         var data = {a: 3, b: 20, c:30, d:8, e:12}
 
@@ -44,7 +43,12 @@ func getPieChart() {
         var color = d3.scaleOrdinal()
           .domain(data)
           .range(["#000", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
-
+          console.log(color.range())
+        for (var i = 0; i <= color.range().length; i++) {
+            console.log(color.range()[index])
+          svg.append("circle").attr("cx",200).attr("cy",160).attr("r", 6).style("fill", "#404080")
+          svg.append("text").attr("x", 220).attr("y", 130).text("variable "+i+"").style("font-size", "15px").attr("alignment-baseline","middle")
+        }
         // Compute the position of each group on the pie:
         var pie = d3.pie()
           .value(function(d) {return d.value; })
@@ -62,6 +66,37 @@ func getPieChart() {
           )
           .attr('fill', function(d){ return(color(d.data.key)) })
           .style("opacity", 0.7)
+          .on('click', function(d) {
+              console.log(d)
+                svg
+                .selectAll('path.slice')
+                .each(function(data_ready) {
+                    data_ready._expanded = false
+                })
+                .transition()
+                .duration(500)
+                .attr('transform', function(d){
+                    return 'translate(0,0)';
+                });
+
+                select(this)
+                .transition()
+                .duration(500)
+                .attr('transform', function(d) {
+                    if (!d._expanded) {
+                        d._expanded = true
+                        const a =
+                        d.startAngle + (d.endAngle - d.startAngle) / 2 - Math.PI / 2
+                        const x = Math.cos(a) * 10
+                        const y = Math.sin(a) * 10
+                        return 'translate(' + x + ',' + y + ')'
+                    } else {
+                        d._expanded = false
+                        return 'translate(0,0)'
+                    }
+                })
+            })
+
 
         </script>
     """
