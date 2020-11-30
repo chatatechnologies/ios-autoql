c6b326ea79bccce25fcd4a16ad4e222557400afe
Vicente Rincón <vicente@rinro.com.mx>
2020-11-30 15:04:53 -0600

    Stable Version

--- Example/chata/DemoViewController.swift
+++ Example/chata/DemoViewController.swift
@@ -18,29 +18,8 @@ class DemoViewController: UIViewController, DemoParameterCellDelegate {
     @IBOutlet weak var tbMain: UITableView!
     @IBOutlet weak var vwMain: UIView!
     @IBOutlet weak var scMain: UISegmentedControl!
-    /*      Spira        */
-    /*let loginSection: DemoSectionsModel =
-        DemoSectionsModel(title: "Authentication", arrParameters: [
-            DemoParameter(label: "* Project ID", type: .input, value: "spira-demo3", key: "projectID"),
-            DemoParameter(label: "* User Email", type: .input, value:"vicente@rinro.com.mx", key: "userID", inputType: .mail),
-            DemoParameter(label: "* API key", type: .input, value: "AIzaSyD4ewBvQdgdYfXl3yIzXbVaSyWGOcRFVeU", key: "apiKey"),
-            DemoParameter(label: "* Domain URL", type: .input, value: "https://spira-staging.chata.io", key: "domain"),
-            DemoParameter(label: "* Username", type: .input, value: "admin", key: "username" ),
-            DemoParameter(label: "* Password", type: .input, value: "admin123", key: "password", inputType: .password),
-            DemoParameter(label: "Authenticate", type: .button, key: "login")
-        ])*/
-    /*Accounting Demo*/
+    
     let loginSection: DemoSectionsModel =
-    DemoSectionsModel(title: "Authentication", arrParameters: [
-        DemoParameter(label: "* Project ID", type: .input, value: "accounting-demo", key: "projectID"),
-        DemoParameter(label: "* User Email", type: .input, value:"vicente@rinro.com.mx", key: "userID", inputType: .mail),
-        DemoParameter(label: "* API key", type: .input, value: "AIzaSyDX28JVW248PmBwN8_xRROWvO0a2BWH67o", key: "apiKey"),
-        DemoParameter(label: "* Domain URL", type: .input, value: "https://accounting-demo-staging.chata.io", key: "domain"),
-        DemoParameter(label: "* Username", type: .input, value: "admin", key: "username" ),
-        DemoParameter(label: "* Password", type: .input, value: "admin123", key: "password", inputType: .password),
-        DemoParameter(label: "Authenticate", type: .button, key: "login")
-    ])
-    /*let loginSection: DemoSectionsModel =
     DemoSectionsModel(title: "Authentication", arrParameters: [
         DemoParameter(label: "* Project ID", type: .input, value: "", key: "projectID"),
         DemoParameter(label: "* User Email", type: .input, key: "userID", inputType: .mail),
@@ -49,7 +28,7 @@ class DemoViewController: UIViewController, DemoParameterCellDelegate {
         DemoParameter(label: "* Username", type: .input, key: "username" ),
         DemoParameter(label: "* Password", type: .input, key: "password", inputType: .password),
         DemoParameter(label: "Authenticate", type: .button, key: "login")
-    ])*/
+    ])
     var allSection: [DemoSectionsModel] = []
     override func viewDidLoad() {
         super.viewDidLoad()
